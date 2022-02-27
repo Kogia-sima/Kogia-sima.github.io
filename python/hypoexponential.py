9e5b919df362ddbdd2981843f819f460317856f8
Kogia-sima <orcinus4627@gmail.com>
2022-02-27 16:30:06 +0900

    Add source files

---
+++ python/hypoexponential.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# from matplotlib import rcParams
+from typing import Any
+
+plt.style.use('ggplot')
+
+
+def hypoexponential(x, lam1, lam2) -> Any:
+    if lam1 == lam2:
+        return lam1 * lam2 * x * np.exp(-lam1 * x)
+    coef = lam1 * lam2 / (lam1 - lam2)
+    return coef * (np.exp(-lam2 * x) - np.exp(-lam1 * x))
+
+
+def main() -> None:
+    x = np.linspace(0.0, 0.5, 500)
+
+    params = [
+        [10, 10],
+        [100, 100 / 19],
+        [1000, 1000 / 199],
+    ]
+
+    plt.figure(figsize = (8, 4), dpi = 120)
+
+    for param in params:
+        label = r'$\lambda_1 = {:.1f}, \lambda_2 = {:.1f}$'.format(
+            param[0], param[1]
+        )
+
+        y = hypoexponential(x, param[0], param[1])
+        plt.plot(x, y, label=label)
+
+    plt.legend()
+    plt.savefig('hypoexponential.png')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ source/about/index.md
@@ -0,0 +1,31 @@
+---
+title: about
+layout: about
+date: 2022-02-26 13:51:11
+---
+
+# Profile
+
+出身: 東京都
+学歴: 北海道大学卒
+所属: Persol Works Design Co., Ltd.
+
+# Skills
+
+- Python
+- tensorflow
+- Vue.js
+- Rust
+- Power BI
+- 統計検定準1級
+- Pythonエンジニア認定データ分析試験
+
+# Other facts
+
+- 統計・データサイエンスに興味があります。
+- 開発はバックエンドのほうが好き
+- 週末はサイクリング+観光するのが趣味です。
+
+# Portfolio
+
+[![Portfolio](/images/0-profile-details.svg)](https://github.com/Kogia-sima)
